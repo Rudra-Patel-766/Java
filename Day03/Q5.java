a74acb4f6c2dc56b8c29a5bf1baaad08fe6a3333
Rudra-Patel-766 <rudrapatel762006@gmail.com>
2026-01-30 11:44:26 +0530

    NewCommit

--- Day03/Q5.java
+++ Day03/Q5.java
@@ -1,5 +1,15 @@
 package Day03;
 
+class Sum{
+    int add(int a, int b) {
+        return a + b;
+    }
+}
+
 public class Q5 {
-    
+    public static void main(String[] args) {
+        Sum calc = new Sum();
+        int result = calc.add(10, 20);
+        System.out.println("Sum = " + result);
+    }
 }
